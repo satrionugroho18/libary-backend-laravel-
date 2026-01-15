87460b52ee190b7a9c57588fab4f7ead524f9a2c
satrionugroho18 <satrionugroho1806@gmail.com>
2026-01-15 13:01:24 +0700

    feat: add CRUD books API and protect with sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+
 
 // Endpoint Public (Bisa diakses tanpa login)
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,5 +13,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     
-    // Nanti route buku & transaksi taruh di sini
+    Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('books', BookController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 });
