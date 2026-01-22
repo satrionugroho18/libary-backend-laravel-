068a69e3be9b866a5dad2025927dad86671eb8e4
satrionugroho18 <satrionugroho1806@gmail.com>
2026-01-22 12:42:15 +0700

    feat: menambahkan migration peminjaman

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ Route::post('/login', [AuthController::class, 'login']);
 // Endpoint Protected (Harus login/bawa token)
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    
     Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('books', BookController::class);
     Route::post('/logout', [AuthController::class, 'logout']);
